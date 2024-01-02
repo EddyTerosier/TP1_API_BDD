0835dd3d6c9a9d0597ebca96e0a646b16d621d31
Terosier Eddy <Eddyjohn.terosier@hotmail.com>
2024-01-02 16:48:16 +0100

    CRUD users

--- server.js
+++ server.js
@@ -10,17 +10,54 @@ app.use(express.json());
 
 // ROUTES
 
-app.get("/", (req,res) => {
-    res.status(200, {"cotent-Type": "text/html; charset=UTF-8"}).send("<h1>Home</h1>");
-})
-
-app.get("/users", (req,res) => {
-    db.query("SELECT * FROM user", (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        res.status(200).json(result);
-    });
+app.get("/", (req, res) => {
+  res.status(200, { "cotent-Type": "text/html; charset=UTF-8" }).send("<h1>Home</h1>");
 });
 
-app.listen(8000, (req,res) => console.log("Server is running on port 8000"));
+// READ
+app.get("/users", async (req, res) => {
+    try {
+      const [rows] = await db.query("SELECT * FROM user");
+      res.status(200).json(rows);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Une erreur est survenue lors de la récupération des utilisateurs' });
+    }
+  });
+
+app.get("/users/:id", async (req, res) => {
+    const id = req.params.id;
+    const [rows] = await db.query(`SELECT * FROM user WHERE id=${id}`);
+    res.status(200).json(rows);
+});
+
+// CREATE
+app.post("/users", async (req, res) => {
+  const lastname = req.body.lastname;
+  const firstname = req.body.firstname;
+  const email = req.body.email;
+
+  await db.query(`INSERT INTO user (lastname, firstname, email) VALUES ("${lastname}", "${firstname}", "${email}")`);
+  res.status(200).json("User added");
+});
+
+// UPDATE
+app.put("/users/:id", async (req, res) => {
+    const id = req.params.id;
+    const lastname = req.body.lastname;
+    const firstname = req.body.firstname;
+    const email = req.body.email;
+    
+    await db.query(`UPDATE user SET lastname="${lastname}", firstname="${firstname}", email="${email}" WHERE id=${id}`);
+    res.status(200).json("User updated");
+});
+
+// DELETE
+app.delete("/users/:id", async (req, res) => {
+    const id = req.params.id;
+    await db.query(`DELETE FROM user WHERE id=${id}`);
+    res.status(200).json("User deleted");
+});
+
+
+app.listen(8000, (req, res) => console.log("Server is running on port 8000"));
